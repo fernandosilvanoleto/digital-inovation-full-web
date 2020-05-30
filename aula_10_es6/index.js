68a9bc452240545f20a7d42650359cd313b08029
Fernando Silva Noleto <fernandonoleto17@gmail.com>
2020-05-30 10:14:51 -0300

    Implementando JavaScript.

---
+++ aula_10_es6/index.js
@@ -0,0 +1,11 @@
+function Pessoa(name) {
+    this.name = name;
+}
+
+const p = new Pessoa('Fernando Sikva');
+
+//console.log(p);
+
+const pessoas = Array.of('Fernando', 'Leide', 2);
+
+console.log(pessoas);
