8c0d19af5f3190fdb800fc9c5dd03a3370403d3a
Fernando Silva Noleto <fernandonoleto17@gmail.com>
2020-06-06 18:32:07 -0300

    Conhecendo Rest, Spread Operator e Destructuring.

--- aula_12_es6avancado/js/main.js
+++ aula_12_es6avancado/js/main.js
@@ -86,6 +86,7 @@ const sum1 = (a, b, ... rest) => {
 console.log(sum(5, 7));
 */
 
+/*
 var arr = ['Apple', 'Banana', 'Orange'];
 
 var apple = arr[0];
@@ -97,3 +98,42 @@ var obj = {
 var { name } = obj;
 
 console.log(name);
+*/
+
+//  Introdução aos Iterators
+/*
+const uniqueId = Symbol('Hello');
+const uniqueId2 = Symbol('Hello');
+
+const arr = [1, 2, 3, 4]
+
+const it = arr[Symbol.iterator]();
+
+for (let value of  arr){
+    console.log(value);
+}
+
+*/
+// Introdução ao Generators
+/*
+function* hello() {
+    console.log('Hello');
+    yield 1;
+
+    console.log('from');
+    const value = yield 2;
+
+    console.log(value);
+
+}
+
+const it = hello();
+
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+*/
+
+// TESTES
+
+
